74f1e31464cef26c959df507669796af7c472773
m1 <uzbzarif@gmail.com>
2024-06-27 18:29:58 +0500

    face detectin

---
+++ download_haar.sh
@@ -0,0 +1,3 @@
+# DOwnload Haar Cascade Classifier for face detection
+
+curl -o haarcascade_frontalface_default.xml https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
